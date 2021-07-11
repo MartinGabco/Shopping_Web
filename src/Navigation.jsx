f3dc2bbcdab690917e8e0a0cd7779132e63f95d8
MartinGabco <gabcomartin@gmail.com>
2021-07-11 13:14:07 +0200

    Badge in Navigation component established - for showing number of product items rendered in Shopping_Basket component.

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 //styles
 import './styles/Navigation.css';
 
 const Navigation = ({ products, forum, shopping_basket }) => {
+
+    const quantityVariable = useSelector((state) => state.basket.addedProducts);
+    const totalQuantityArray = quantityVariable.map(item => item.quantity);
+
+    // Quantity sum
+    let totalQuantitySum = 0;
+    for (let i = 0; i < totalQuantityArray.length; i++) {
+        totalQuantitySum += totalQuantityArray[i];
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <NavLink className="nav-item nav-link font-weight-light" to="/" exact><a>Produkty</a></NavLink>
                     <NavLink className="nav-item nav-link font-weight-light" to="/forum"><a>Fórum</a></NavLink>
-                    <NavLink className="nav-item nav-link font-weight-light" to="/shopping_basket"><a>Košík</a></NavLink>
+                    <NavLink className="nav-item nav-link font-weight-light" to="/shopping_basket">
+                        <a>Košík</a>
+                        <span class="badge badge-pill badge-primary">{totalQuantitySum}</span>
+                    </NavLink>
                 </div>
             </div>
         </nav>
